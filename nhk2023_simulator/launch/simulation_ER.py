f788ac075d89d1fbe1cb9b4734a09f7d1aa8beae
curious-jp <curious.ks.jp@gmail.com>
2022-10-22 22:23:41 +0900

    launcher周りを編集、urdfに変更途中だが明日は体育館なので一旦commit

--- nhk2023_simulator/launch/simulation_ER.py
+++ nhk2023_simulator/launch/simulation_ER.py
@@ -1,43 +1,44 @@
 
-        #  ...                                     ..:.
-        # :..:::::.......               ........:::::..:
-        # .::..:::::::::::.            .:::::::::::..::
-        #  :::..:::::::::::.         .:::::::::::..:::.
-        #  .::::   .:::::::::.      .:::::::::.  .::::
-        #  .::::.  :::::::::::.   .:::::::::::. .:::::
-        #   ::::::::::::::::::::. :::::::::::::::::::.
-        #   :::::::::::::::::::::. ::::::::::::::::::.
-        #  .:::::::::::::::::::::::..:::::::::::::::::
-        #    .::::::::::::::::::::::. ::::::::::::::.
-        #      .::::::::::::::::::::::..::::::::::.
-        #        .:::::::::::::::::::::. :::::::.
-        #          .:::::::::::::::::::::..:::.
-        #            .::::::::::::::::::::. .
-        #            ...::::::::::::::::::::.
-        #          .:::...:::::::::::::::::::.
-        #         .::::::...:::::::::::::::::::.
-        #       .::::::::::...::::::::::::::::::.
-        #      ::::::::::::::...::::::::::::::::::.
-        #    .:::::::::::::::::. .::::::::::::::::::
-        #   ::::::::::::::::::.    .:::::::::::::::::.
-        #  :::::::::::::::::.        .::::::::::::::::.
-        #  :::::::::::::::.            .::::::::::::::.
-        #  :::::::::::::.                .::::::::::::.
-        #   ::::::::::.                    .:::::::::.
-        #    ..::::..                        .:::::.
-
-
-# Path: nhk2023_ilias/nhk2023_simulator/launch/simulation_ER.py
+#  ...                                     ..:.
+# :..:::::.......               ........:::::..:
+# .::..:::::::::::.            .:::::::::::..::
+#  :::..:::::::::::.         .:::::::::::..:::.
+#  .::::   .:::::::::.      .:::::::::.  .::::
+#  .::::.  :::::::::::.   .:::::::::::. .:::::
+#   ::::::::::::::::::::. :::::::::::::::::::.
+#   :::::::::::::::::::::. ::::::::::::::::::.
+#  .:::::::::::::::::::::::..:::::::::::::::::
+#    .::::::::::::::::::::::. ::::::::::::::.
+#      .::::::::::::::::::::::..::::::::::.
+#        .:::::::::::::::::::::. :::::::.
+#          .:::::::::::::::::::::..:::.
+#            .::::::::::::::::::::. .
+#            ...::::::::::::::::::::.
+#          .:::...:::::::::::::::::::.
+#         .::::::...:::::::::::::::::::.
+#       .::::::::::...::::::::::::::::::.
+#      ::::::::::::::...::::::::::::::::::.
+#    .:::::::::::::::::. .::::::::::::::::::
+#   ::::::::::::::::::.    .:::::::::::::::::.
+#  :::::::::::::::::.        .::::::::::::::::.
+#  :::::::::::::::.            .::::::::::::::.
+#  :::::::::::::.                .::::::::::::.
+#   ::::::::::.                    .:::::::::.
+#    ..::::..                        .:::::.
 
 import os
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 from ament_index_python.packages import get_package_share_directory
 
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
     nhk2023_simulator = get_package_share_directory('nhk2023_simulator')
@@ -47,28 +48,32 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
         launch_arguments={
-            'gz_args': '-r nhk2023.world'
-        }.items()
+            'gz_args': '-r worlds/nhk2023.world --render-engine ogre'
+            #WSLで動かせるようにogreを指定しているが、他の環境では不要
+        }.items(),
     )
 
     rviz = Node(
-            package='rviz2',
-            executable='rviz2',
-            arguments=['d', os.path.join(nhk2023_simulator, 'config/rviz/simulation.rviz')],
-            condition=IfCondition(LaunchDescription('rviz'))
+        package='rviz2',
+        executable='rviz2',
+        arguments=['d', os.path.join(nhk2023_simulator, 'config/rviz/simulation.rviz')],
+        #rvizの設定ファイルを指定、モード毎に読み込むファイルを変えるみたいなのがあっても良いかも
+        condition=IfCondition(LaunchConfiguration('rviz'))
     )
 
     bridge = Node(
-            package='ros_gz_bridge',
-            executable='parameter_bridge',
-            arguments=['/nhk2023/robot_description', '/robot_description'],
-            parameters=[os.path.join(nhk2023_simulator, 'config/parameter_bridge.yaml')],
-            output='screen'
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/navsat@sensor_msgs/msg/NavSatFix@ignition.msgs.NavSat'],
+        #ここに追加すると、rosとignitionの間でデータをやり取りできる、現状は仮置き
+        output='screen'
     )
 
     return LaunchDescription([
         gz_sim,
+        DeclareLaunchArgument('rviz', default_value='true',
+                              description='Open RViz.'),
         rviz,
-        bridge,
-        DeclareLaunchArgument('rviz', default_value='true',description='Open RViz.')
+        bridge
     ])
+
