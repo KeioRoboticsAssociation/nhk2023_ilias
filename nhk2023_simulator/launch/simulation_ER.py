c1f3f05b0f946e7587bdca06283fb3714fe8c0c3
curious-jp <curious.ks.jp@gmail.com>
2022-11-20 01:17:08 +0900

    シミュレーター周りのメッセージのやり取りやらを実装、まだ未完です

--- nhk2023_simulator/launch/simulation_ER.py
+++ nhk2023_simulator/launch/simulation_ER.py
@@ -1,48 +1,17 @@
-
-#  ...                                     ..:.
-# :..:::::.......               ........:::::..:
-# .::..:::::::::::.            .:::::::::::..::
-#  :::..:::::::::::.         .:::::::::::..:::.
-#  .::::   .:::::::::.      .:::::::::.  .::::
-#  .::::.  :::::::::::.   .:::::::::::. .:::::
-#   ::::::::::::::::::::. :::::::::::::::::::.
-#   :::::::::::::::::::::. ::::::::::::::::::.
-#  .:::::::::::::::::::::::..:::::::::::::::::
-#    .::::::::::::::::::::::. ::::::::::::::.
-#      .::::::::::::::::::::::..::::::::::.
-#        .:::::::::::::::::::::. :::::::.
-#          .:::::::::::::::::::::..:::.
-#            .::::::::::::::::::::. .
-#            ...::::::::::::::::::::.
-#          .:::...:::::::::::::::::::.
-#         .::::::...:::::::::::::::::::.
-#       .::::::::::...::::::::::::::::::.
-#      ::::::::::::::...::::::::::::::::::.
-#    .:::::::::::::::::. .::::::::::::::::::
-#   ::::::::::::::::::.    .:::::::::::::::::.
-#  :::::::::::::::::.        .::::::::::::::::.
-#  :::::::::::::::.            .::::::::::::::.
-#  :::::::::::::.                .::::::::::::.
-#   ::::::::::.                    .:::::::::.
-#    ..::::..                        .:::::.
-#       ..                              .:
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+# from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    nhk2023_simulator = get_package_share_directory('nhk2023_simulator')
+
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
     gz_sim = IncludeLaunchDescription(
@@ -54,27 +23,46 @@ def generate_launch_description():
         }.items(),
     )
 
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        arguments=['d', os.path.join(nhk2023_simulator, 'config/rviz/simulation.rviz')],
-        #rvizの設定ファイルを指定、モード毎に読み込むファイルを変えるみたいなのがあっても良いかも
-        condition=IfCondition(LaunchConfiguration('rviz'))
-    )
+    # nhk2023_simulator = get_package_share_directory('nhk2023_simulator')
+    # rviz = Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     arguments=['d', os.path.join(nhk2023_simulator, 'config/rviz/simulation.rviz')],
+    #     #rvizの設定ファイルを指定、モード毎に読み込むファイルを変えるみたいなのがあっても良いかも
+    # )
+    # # rvizはメインのlaunchファイルで起動するので、ここではコメントアウト
 
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=[''],
-        #ここに追加すると、rosとignitionの間でデータをやり取りできる、現状は仮置き
-        output='screen'
+        arguments=[
+            '/world/nhk2023_field/model/odm_robot/link/base_link/sensor/lidar/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan', #Lidar
+            '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU', #IMU
+            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist', #cmd_vel
+            '/model/odm_robot/pose@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V' #odm_robotの位置情報
+            #ここに追加すると、rosとignitionの間でデータをやり取りできるようになる
+            ],
+        output='screen',
+        remappings=[
+                ('/world/nhk2023_field/model/odm_robot/link/base_link/sensor/lidar/scan', 'lidar/scan'),
+                ('/model/odm_robot/pose', '/tf'),
+                #ここでremappingすると、rosのトピック名を変えられる
+            ],
     )
+
+    tf_publisher = Node(
+        package= 'nhk2023_simulator',
+        executable='odom_tf_publisher',
+        output='screen',
+    )
+
 
     return LaunchDescription([
         gz_sim,
-        DeclareLaunchArgument('rviz', default_value='true',
-                              description='Open RViz.'),
-        rviz,
-        bridge
+        # DeclareLaunchArgument('rviz', default_value='true',
+        #                       description='Open RViz.'),
+        # rviz,
+        bridge,
+        tf_publisher,
     ])
 
