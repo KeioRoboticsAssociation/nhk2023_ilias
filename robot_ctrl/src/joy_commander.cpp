bd3a0c062f0ddca931ecd39ef80aab90e3b35c05
curious-jp <curious.ks.jp@gmail.com>
2022-12-02 01:21:42 +0900

    色々ミスってる、寝る

--- robot_ctrl/src/joy_commander.cpp
+++ robot_ctrl/src/joy_commander.cpp
@@ -1,55 +1,44 @@
 // This node publishes a cmd_vel topic to the robot.
 //  The topic is subscribed by the robot and the robot moves according to the
 //  message.
-
-#include "joy_commander.hpp"
-
 #include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joy.hpp>
 
-class JoyCommander : public rclcpp::Node {
+class JoyCommander {
  public:
-  JoyCommander();
-  geometry_msgs::msg::Twist cmd_vel;
+  JoyCommander(){};
+  static geometry_msgs::msg::Twist joy_cmd_vel;
 
- private:
-  void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
+  enum class button {
+    A = 0,
+    B = 1,
+    X = 2,
+    Y = 3,
+    LB = 4,
+    RB = 5,
+    BACK = 6,
+    START = 7,
+    L3 = 8,
+    R3 = 9,
+  };
 
-  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+  enum class axis {
+    LX = 0,
+    LY = 1,
+    LT = 2,
+    RX = 3,
+    RY = 4,
+    RT = 5,
+    DPAD_X = 6,
+    DPAD_Y = 7,
+  };
 
-  // timer in 10Hz
-  rclcpp::TimerBase::SharedPtr timer_;
-  void timer_callback();
+  void joy_callback(const sensor_msgs::msg::Joy::SharedPtr &msg);
 };
 
-JoyCommander::JoyCommander() : Node("joy_commander") {
-  RCLCPP_INFO(this->get_logger(), "joy_commander node is started");
-
-  joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
-      "joy", 10,
-      std::bind(&JoyCommander::joy_callback, this, std::placeholders::_1));
-
-  cmd_vel_pub_ =
-      this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-
-  timer_ =
-      this->create_wall_timer(std::chrono::milliseconds(100),
-                              std::bind(&JoyCommander::timer_callback, this));
-}
-
-void JoyCommander::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
-  cmd_vel.linear.x = msg->axes[static_cast<int>(JoyCommander::axes::LY)];
-  cmd_vel.linear.y = msg->axes[static_cast<int>(JoyCommander::axes::LX)];
-  cmd_vel.angular.z = msg->axes[static_cast<int>(JoyCommander::axes::RX)];
-}
-
-void JoyCommander::timer_callback() { cmd_vel_pub_->publish(cmd_vel); }
-
-int main(int argc, char *argv[]) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<JoyCommander>());
-  rclcpp::shutdown();
-  return 0;
+void JoyCommander::joy_callback(const sensor_msgs::msg::Joy::SharedPtr &msg) {
+  joy_cmd_vel.linear.x = msg->axes[static_cast<int>(JoyCommander::axis::LY)];
+  joy_cmd_vel.linear.y = msg->axes[static_cast<int>(JoyCommander::axis::LX)];
+  joy_cmd_vel.angular.z = msg->axes[static_cast<int>(JoyCommander::axis::RX)];
 }
