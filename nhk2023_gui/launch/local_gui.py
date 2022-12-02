257dfcae8c11fef7de1f1c66981a8048d5bf7b0f
curious-ThinkPad <curious.ks.jp@gmail.com>
2022-12-02 17:52:51 +0900

    雛形作成

---
+++ nhk2023_gui/launch/local_gui.py
@@ -0,0 +1,21 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+# from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+
+
+
+    return LaunchDescription([
+
+    ])
+
