758f437086106e70d482b2b5e0f734351dff3a9e
curious-jp <curious.ks.jp@gmail.com>
2022-12-10 01:10:28 +0900

    途中まで実装、なんかこれ上手くいかない？

--- local_gui/local_gui/local_gui.py
+++ local_gui/local_gui/local_gui.py
@@ -1,42 +1,116 @@
-import tkinter
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import String
+import tkinter as tk
+from tkinter import ttk
 
 class LocalGui:
-    def __init__(self):
-        self.root = tkinter.Tk()
-        self.root.title("Local GUI")
-        self.root.geometry("300x200")
-        self.label = tkinter.Label(font=("Ubunt Mono", 160))
-        self.label.pack()
+    def __init__(self,root):
+        self.root = root
+        # self.root.title("Local GUI")
 
-    tmr = 0 # カウンタ変数
+    style_unpressed = ttk.Style()
+    style_unpressed.configure(
+        "unpressed.TButton",
+        background = "gray",
+        foreground = "black",
+        font = ("", 40),
+        relief = "flat",
+        borderwidth = 0,
+        padding = 0
+    )
 
-    def update(self):
-        global tmr
-        self.tmr = self.tmr + 1
-        self.label["text"] = self.tmr
-        self.root.after(10, self.update) # 1秒後にcounter関数を呼び出す
+    style_pressed = ttk.Style()
+    style_pressed.configure(
+        "pressed.TButton",
+        background = "black",
+        foreground = "black",
+        font = ("", 40),
+        relief = "flat",
+        borderwidth = 0,
+        padding = 0
+    )
 
-class LocalGuiNode(Node):
-    def __init__(self):
-        super().__init__('local_gui_node')
-        self.subscription = self.create_subscription(
-            String,
-            'mode',
-            self.mode_callback,
-            10)
-        self.local_gui = LocalGui()
+    def start_window(self):
 
-    def mode_callback(self, msg):
-        self.local_gui.display_mode(msg.data)
+        # self.root.place(x = 0, y = 0)
+        self.b0 = ttk.Button(
+            self.root,
+            text = "AUTO",
+            command = self.button_clicked,
+            style = "unpressed.TButton"
+            )
+        self.b0.pack()
 
-def main(args=None):
-    local_gui = LocalGui()
-    local_gui.update()
-    local_gui.root.mainloop()
+        self.b1 = ttk.Button(
+            self.root,
+            text = "MANUAL",
+            command = self.button_clicked,
+            style="unpressed.TButton"
+            )
+        self.b1.pack()
+
+        self.b2 = ttk.Button(
+            self.root,
+            text = "PRECISION",
+            command = self.button_clicked,
+            style="unpressed.TButton"
+            )
+        self.b2.pack()
+
+        self.b3 = ttk.Button(
+            self.root,
+            text = "CLIMB",
+            command = self.button_clicked,
+            style="unpressed.TButton"
+            )
+        self.b3.pack()
+
+        # self.b0.place(
+        #     x = 136, y = 84,
+        #     width = 385,
+        #     height = 174)
+        # self.b1.place(
+        #     x = 557, y = 84,
+        #     width = 385,
+        #     height = 174)
+        # self.b2.place(
+        #     x = 978, y = 84,
+        #     width = 385,
+        #     height = 174)
+        # self.b3.place(
+        #     x = 1399, y = 84,
+        #     width = 385,
+        #     height = 174)
+
+        self.root.resizable(True, True)
+        self.root.mainloop()
+
+    # change button style when clicked
+    def button_clicked(self):
+        if self.b0["style"] == "unpressed.TButton":
+            self.b0["style"] = "pressed.TButton"
+        else:
+            self.b0["style"] = "unpressed.TButton"
+
+        if self.b1["style"] == "unpressed.TButton":
+            self.b1["style"] = "pressed.TButton"
+        else:
+            self.b1["style"] = "unpressed.TButton"
+
+        if self.b2["style"] == "unpressed.TButton":
+            self.b2["style"] = "pressed.TButton"
+        else:
+            self.b2["style"] = "unpressed.TButton"
+
+        if self.b3["style"] == "unpressed.TButton":
+            self.b3["style"] = "pressed.TButton"
+        else:
+            self.b3["style"] = "unpressed.TButton"
 
 
 if __name__ == "__main__":
-    main()
+    app = tk.Tk()
+    # app.geometry("1980x1080")
+    window = LocalGui(app)
+    window.start_window()
+    app.mainloop()
+
+
