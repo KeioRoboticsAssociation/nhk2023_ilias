774f4339ae716249ef7da52a693358d520fff2a2
long-paddy-field <84272166+long-paddy-field@users.noreply.github.com>
2022-12-31 23:21:59 +0900

    CREATE: package Poll_Detector

---
+++ poll_detection/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'poll_detection'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yugoosada',
+    maintainer_email='84272166+long-paddy-field@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
