8f3b10522d068ad084e7acb9a2010b086fdd1cdd
curious-jp <curious.ks.jp@gmail.com>
2023-01-07 00:23:50 +0900

    iroirokaeta

--- nhk2023_simulator/launch/simulation_RR.launch.py
+++ nhk2023_simulator/launch/simulation_RR.launch.py
@@ -6,6 +6,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import SetEnvironmentVariable
 
 from launch_ros.actions import Node
 import xacro
@@ -13,15 +14,14 @@ import xacro
 
 def generate_launch_description():
 
-    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
-    world_file_name = 'nhk2023.sdf'
+    # use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    # world_file_name = 'model.sdf'
     pkg_dir = get_package_share_directory('nhk2023_simulator')
 
-    os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'models')
+    model_path = os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'models') + ":" + os.path.join(pkg_dir, 'worlds')
+    # world = os.path.join(pkg_dir, 'worlds', 'field2023', world_file_name)
 
-    world = os.path.join(pkg_dir, 'worlds', world_file_name)
-
-    xacro_file = os.path.join(pkg_dir, 'models', 'rr.xacro')
+    xacro_file = os.path.join(pkg_dir, 'models','rr', 'rr.xacro')
     doc = xacro.parse(open(xacro_file))
     xacro.process_doc(doc)
     params = {'robot_description': doc.toxml()}
@@ -37,14 +37,20 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join
             (get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-        launch_arguments={'world': world, 'verbose': 'true', 'use_sim_time': use_sim_time}.items(),
+        # launch_arguments={'world': world, 'verbose': 'true', 'use_sim_time': use_sim_time}.items(),
     )
+
+
+
 
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-entity', 'rr', '-topic', 'robot_description', '-x', '0', '-y', '0', '-z', '0.1'],
                         output='screen')
 
+
+
     return LaunchDescription([
+        SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value = model_path),
         gazebo,
         spawn_entity,
         node_robot_state_publisher,
