247243cf3b59664fbdb53b4a037ebf63f59578ff
curious-ThinkPad <curious.ks.jp@gmail.com>
2023-01-07 19:50:31 +0900

    うーんうまくいきませんねえ

---
+++ nhk2023_simulator/launch/simulation_ER.launch.py
@@ -0,0 +1,70 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+# from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={
+            'gz_args': '-r worlds/nhk2023.world --render-engine ogre'
+            #WSLで動かせるようにogreを指定しているが、他の環境では不要
+        }.items(),
+    )
+
+    # nhk2023_simulator = get_package_share_directory('nhk2023_simulator')
+    # rviz = Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     arguments=['d', os.path.join(nhk2023_simulator, 'config/rviz/simulation.rviz')],
+    #     #rvizの設定ファイルを指定、モード毎に読み込むファイルを変えるみたいなのがあっても良いかも
+    # )
+    # # rvizはメインのlaunchファイルで起動するので、ここではコメントアウト
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/world/nhk2023_field/model/er/link/base_link/sensor/lidar/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan', #Lidar
+            '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU', #IMU
+            # '/world/nhk2023_field/model/er/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist', #cmd_vel
+            '/model/er/pose@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V' #odm_robotの位置情報
+            '/'
+            #ここに追加すると、rosとignitionの間でデータをやり取りできるようになる
+            ],
+        output='screen',
+        remappings=[
+                ('/world/nhk2023_field/model/er/link/base_link/sensor/lidar/scan', '/er/lidar/scan'),
+                ('/model/er/pose', '/er/pose'),
+                ('/world/nhk2023_field/model/er/cmd_vel','/er/cmd_vel')
+                #ここでremappingすると、rosのトピック名を変えられる
+            ],
+    )
+
+    odom_publisher = Node(
+        package= 'nhk2023_simulator',
+        executable='er_odom_publisher',
+        output='screen',
+    )
+
+
+    return LaunchDescription([
+        gz_sim,
+        # DeclareLaunchArgument('rviz', default_value='true',
+        #                       description='Open RViz.'),
+        # rviz,
+        bridge,
+        odom_publisher,
+    ])
+
---
+++ nhk2023_simulator/launch/simulation_RR.launch.py
@@ -0,0 +1,69 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import SetEnvironmentVariable
+from launch.actions import ExecuteProcess
+
+from launch_ros.actions import Node
+import xacro
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    world_file_name = 'model.world'
+    pkg_dir = get_package_share_directory('nhk2023_simulator')
+
+    world = os.path.join(pkg_dir, 'worlds', 'field2023' , world_file_name)
+
+    # if 'GAZEBO_MODEL_PATH' in os.environ:
+    #     model_path =  os.environ['GAZEBO_MODEL_PATH'] + ':' +  os.path.join(pkg_dir, 'models') + ":" + os.path.join(pkg_dir, 'worlds')
+    # else:
+    #     model_path =   os.path.join(pkg_dir, 'models') + ":" + os.path.join(pkg_dir, 'worlds')
+
+    # os.environ['GAZEBO_MODEL_PATH'] = os.path.join(pkg_dir, 'models') + ":" + os.path.join(pkg_dir, 'worlds')
+
+    xacro_file = os.path.join(pkg_dir, 'models','rr', 'rr.xacro')
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml()}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    # include launch file from gazebo_ros package
+    # gazebo = ExecuteProcess(
+    #         cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so',
+    #         '-s', 'libgazebo_ros_factory.so'],
+    #         output='screen')
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([get_package_share_directory('gazebo_ros'), '/launch', '/gazebo.launch.py']),
+        launch_arguments={'world': world, 'verbose': 'true', 'debug': 'true', 'gui': 'true', 'paused': 'false', 'use_sim_time': use_sim_time}.items(),
+    )
+
+
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-entity', 'rr', '-topic', 'robot_description', '-x', '0', '-y', '0', '-z', '0.1'],
+                        output='screen')
+
+
+
+    return LaunchDescription([
+        # SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value = model_path),
+        # ExecuteProcess(
+        #     cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+        #     output='screen'),
+        gazebo,
+        # spawn_entity,
+        node_robot_state_publisher,
+    ])
