8767d16f21dfd9970978abd9e5d2a5c1d734ca40
keiorogiken <keiorogiken@gmail.com>
2023-02-19 15:59:41 +0900

    sokudo koushin

--- robot_ctrl/src/rr/robot_ctrl.cpp
+++ robot_ctrl/src/rr/robot_ctrl.cpp
@@ -47,8 +47,8 @@ robot_ctrl::robot_ctrl()
   RCLCPP_INFO(this->get_logger(), "robot_ctrl node is started");
 
   // set parameters
-  this->declare_parameter("max_linear_vel", 12.0);
-  this->declare_parameter("max_angular_vel", 3.0);
+  this->declare_parameter("max_linear_vel", 5.0);
+  this->declare_parameter("max_angular_vel", 2.0);
   this->declare_parameter("max_linear_acc", 3.0);
   this->declare_parameter("max_angular_acc", 3.0);
 
