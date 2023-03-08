1fe14c2d2cb9d458918cac76299c0a3d69dd3013
curious-ThinkPad <curious.ks.jp@gmail.com>
2023-03-08 19:16:50 +0900

    iroirostate追加

---
+++ state_ctrl/rr_state_ctrl/src/end.cpp
@@ -0,0 +1,11 @@
+#include "robot_state_ctrl.hpp"
+#include "state_ctrl.hpp"
+
+void End::entry(void) {
+  RCLCPP_INFO(robot_state_ctrl->get_logger(), "End entry");
+  // publish mode message
+  auto msg = std_msgs::msg::String();
+  msg.data = "END";
+  robot_state_ctrl->mode_pub_->publish(msg);
+  RCLCPP_INFO(robot_state_ctrl->get_logger(), "mode msg end");
+}
---
+++ state_ctrl/rr_state_ctrl/src/restart.cpp
@@ -0,0 +1,11 @@
+#include "robot_state_ctrl.hpp"
+#include "state_ctrl.hpp"
+
+void Restart::entry(void) {
+  RCLCPP_INFO(robot_state_ctrl->get_logger(), "Restart entry");
+  // publish mode message
+  auto msg = std_msgs::msg::String();
+  msg.data = "RESTART";
+  robot_state_ctrl->mode_pub_->publish(msg);
+  RCLCPP_INFO(robot_state_ctrl->get_logger(), "mode msg restart");
+}
