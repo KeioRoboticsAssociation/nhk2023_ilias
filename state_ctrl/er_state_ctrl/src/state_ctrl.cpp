28c5b22a3e0a929d5649b693f90a377a36e24f04
curious-ThinkPad <curious.ks.jp@gmail.com>
2023-03-28 15:30:39 +0900

    launcherだけにした

--- state_ctrl/er_state_ctrl/src/state_ctrl.cpp
+++
@@ -1,8 +0,0 @@
-#include "state_ctrl.hpp"
-
-#include "robot_state_ctrl.hpp"
-
-StateMachine::StateMachine() {
-  if (robot_state_ctrl == NULL) return;
-  RCLCPP_INFO(robot_state_ctrl->get_logger(), "StateMachine constructor");
-}
