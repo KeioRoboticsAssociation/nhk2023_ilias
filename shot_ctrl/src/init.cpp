ed78580041ae474adf4e753cff4e367dfd64b234
Oyaki122 <oc.yaki122@gmail.com>
2023-04-15 17:03:42 +0900

    initとloadLoaderを仮実装

---
+++ shot_ctrl/src/init.cpp
@@ -0,0 +1,69 @@
+#include "../include/shot_ctrl/node.hpp"
+#include "../include/shot_ctrl/shot_state.hpp"
+
+void Init::entry() {
+  logInfo("Enter Init");
+
+  if (!limitSensor->getPinState(MAGAZIN_LIMIT)) {
+    limitSensor->addCallback(MAGAZIN_LIMIT, [&]() {
+      magazin->setVoltage(0);
+      magazin->resetEncoder(0);
+      magazin->setMode(MD2022::Position);
+      magazin->setPosition(0);
+    });
+    magazin->setMode(MD2022::Voltage);
+    magazin->setVelocity(0.1);
+  }
+  if (!limitSensor->getPinState(PUSHER_LIMIT)) {
+    limitSensor->addCallback(PUSHER_LIMIT, [&]() {
+      pusher->setVoltage(0);
+      pusher->resetEncoder(0);
+      pusher->setMode(MD2022::Position);
+      pusher->setPosition(0);
+    });
+    pusher->setMode(MD2022::Voltage);
+    pusher->setVelocity(0.1);
+  }
+  if (!limitSensor->getPinState(LOADER_LIMIT)) {
+    limitSensor->addCallback(LOADER_LIMIT, [&]() {
+      loader->setVoltage(0);
+      loader->resetEncoder(0);
+      loader->setMode(MD2022::Position);
+      loader->setPosition(0);
+    });
+    loader->setMode(MD2022::Voltage);
+    loader->setVelocity(0.1);
+  }
+  if (!limitSensor->getPinState(ELEVATION_LIMIT)) {
+    limitSensor->addCallback(ELEVATION_LIMIT, [&]() {
+      elevation->setVoltage(0);
+      elevation->resetEncoder(0);
+      elevation->setMode(MD2022::Position);
+      elevation->setPosition(0);
+    });
+    elevation->setMode(MD2022::Voltage);
+    elevation->setVelocity(0.1);
+  }
+  if (!limitSensor->getPinState(SHOOTER_LIMIT)) {
+    limitSensor->addCallback(SHOOTER_LIMIT, [&]() {
+      shooter->setVelocity(0);
+      shooter->resetEncoder(0);
+      shooter->setMode(ODrive::Position,
+                       ODriveEnum::InputMode::INPUT_MODE_TRAP_TRAJ);
+      shooter->setPosition(0);
+    });
+    shooter->setMode(ODrive::Velocity);
+    shooter->setVelocity(0.1);
+  }
+}
+
+void Init::react(UpdateEvent const &) {}
+
+void Init::react(MagazinLoadedEvent const &e) {
+  if (e.isLeft == Context::LEFT) {
+    context.leftRemain = 10;
+  } else {
+    context.rightRemain = 10;
+  }
+  transit<LoadMagazine>();
+}
--- shot_ctrl/src/loadLoader.cpp
+++ shot_ctrl/src/loadLoader.cpp
@@ -1,13 +1,15 @@
 #include "../include/shot_ctrl/shot.hpp"
 #include "../include/shot_ctrl/shot_state.hpp"
 
+const float pusherLoadPosition = 5;
+
 void LoadLoader::entry() {
   logInfo("Enter LoadLoader");
-  loader->setPosition(5);
+  pusher->setPosition(pusherLoadPosition);
 }
 
 void LoadLoader::react(UpdateEvent const &) {
-  if (loader->getPosition() >= 4.9) {
+  if (pusher->getPosition() >= pusherLoadPosition - 0.1) {
     transit<DownLoader>();
   }
 }
